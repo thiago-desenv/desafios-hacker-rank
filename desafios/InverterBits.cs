f23f903747ba81d5d25e3161f2019bcb947fc9e7
thiago_osim <thiago_osim@hotmail.com>
2024-09-11 21:38:30 -0300

    Solução do desafio inverter bits

---
+++ desafios/InverterBits.cs
@@ -0,0 +1,12 @@
+﻿namespace desafios_hacker_rank.desafios
+{
+    public class InverterBits
+    {
+        public static void TransformarBits()
+        {
+            long valor = 4294967294;
+            uint valorComBitsInvertido = (uint)~valor;
+            Console.WriteLine("valor com os bits invertidos: " + valorComBitsInvertido);
+        }
+    }
+}
