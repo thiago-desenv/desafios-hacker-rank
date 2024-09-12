a7da4d09ff27fc726a2c6eec1a493bf1b69a056b
thiago_osim <thiago_osim@hotmail.com>
2024-09-11 21:31:29 -0300

    Solução do desafio encontrar elemento único

---
+++ desafios/ElementoUnico.cs
@@ -0,0 +1,35 @@
+﻿namespace desafios_hacker_rank.desafios
+{
+    public class ElementoUnico
+    {
+        public static void EncontrarValorUnico()
+        {
+            List<int> valores = new List<int> { 1, 1, 2, 2, 10 };
+
+            Dictionary<int, int> chavesValores = new Dictionary<int, int>();
+            int valorUnico = 0;
+
+            foreach(int item in valores)
+            {
+                if(chavesValores.ContainsKey(item))
+                {
+                    chavesValores[item]++;
+                }
+                else
+                {
+                    chavesValores[item] = 1;
+                }
+            }
+
+            foreach(var chaveValor in chavesValores)
+            {
+                if(chaveValor.Value == 1)
+                {
+                    valorUnico = chaveValor.Key;
+                }
+            }
+
+            Console.WriteLine($"Valor único: {valorUnico}");
+        }
+    }
+}
